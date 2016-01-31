6bcff6d61753fd7506e62f6ad25b40738e4d6f35
Stewart Olson <stewart.olson@gmail.com>
2016-01-30 19:47:46 -0800

    Chapter2 Stuff
    Basic TCP Client
    Basic UDP Client
    Basic TCP Server

---
+++ Black Hat Python/ch2_TCP_Client.py
@@ -0,0 +1,19 @@
+import socket
+
+target = "google.com"
+port = 80
+
+# create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect the client to the server
+client.connect((target, port))
+
+# send some data to server
+client.send("My first TCP Client")
+
+# receive some data from the server
+response = client.recv(4096)
+# Find out why the value here is 4096?
+
+print response
