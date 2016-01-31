f211d1e43f9fb79e98ee74725bad5514c4970d31
Stewart Olson <stewart.olson@gmail.com>
2016-01-30 22:12:21 -0800

    Some more of my old LPTHW exercises.

---
+++ Learn Python the Hard Way/ex05.py
@@ -0,0 +1,20 @@
+name = 'Stewart Olson'
+age = 27
+height_in = 72 # inches
+height_cm = height_in * 2.54 # centimeters
+weight_lb = 170 # pounds
+weight_kg = weight_lb * 0.4536 # kilograms
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Black'
+
+print "Let's talk about %r." % name
+print "He's %r inches, %r centimeters tall." % (height_in, height_cm)
+print "He's %r pounds, %r kilograms heavy." % (weight_lb, weight_kg)
+print "Actually that's not too heavy."
+print "He's got %r eyes and %r hair." % (eyes, hair)
+print "His teeth are usually %r depending on brushing frequency." % teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %r, %r, and %r I get %r." % (
+    age, height_in, weight_lb, age + height_in + weight_lb)
